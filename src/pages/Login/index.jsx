779763dffd8be57143a90f92cc9a16a54663f79e
Fernando Laterza <fhlaterza@gmail.com>
2024-11-04 16:29:26 -0300

    correção input

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -55,7 +55,6 @@ const Login= () => {
                         <CriarText>Criar conta</CriarText>
                     </Row>
                 </Wrapper>
-                <Input placeholder="email"/>                
             </Column>
         </Container>
     </>)
